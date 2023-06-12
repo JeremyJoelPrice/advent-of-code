ca089f7a0700cdc92654bdb30dd6cb20fd1c86d1
Jeremy Joel Price <JeremyJoelPrice@gmail.com>
2023-06-12 12:43:09 +0100

    solve day 8 problem 2

--- day-8/go.js
+++ day-8/go.js
@@ -10,13 +10,11 @@ const directions = {
 
 // program
 getData("./day-8/data.txt").then((data) => {
+	const forest = getArrayOfForest(data);
 	// solution 1:
-	// const forest = getArrayOfForest(data);
 	// console.log(countVisibleTreesInForest(forest));
 	// solution 2:
-	// console.log(forest);
-	// get the scenic score for every inner tree
-	// return the highest scenic score
+	// console.log(getHighestScenicScoreInForest(forest));
 });
 
 function countVisibleTreesInForest(forest) {
@@ -38,7 +36,7 @@ function countVisibleTreesInForest(forest) {
 
 function forEachTree(forest, callback) {
 	for (let ty = 0; ty < forest.length; ty++) {
-		for (let tx = 0; tx < forest.length; tx++) {
+		for (let tx = 0; tx < forest[ty].length; tx++) {
 			callback({ tx, ty });
 		}
 	}
@@ -62,12 +60,12 @@ function isVisibleInDirection(direction, tree, forest) {
 		cx += dx;
 		cy += dy;
 
-		// has the cursor passed the edge of the forest?
+		// if the cursor passed the edge of the forest, return true
 		if (cy < 0 || cy === forest.length || cx < 0 || cx === forest.length) {
 			return true;
 		}
 
-		// is the current tree shorter than the original tree?
+		// if the current tree is not shorter than the original tree, return false
 		if (forest[cy][cx] >= forest[ty][tx]) {
 			return false;
 		}
@@ -79,9 +77,68 @@ function isVisibleInDirection(direction, tree, forest) {
 	return testNextTree({ cx: tx, cy: ty });
 }
 
+function getViewingDistanceInDirection(direction, tree, forest) {
+	// tree under consideration
+	const { tx, ty } = tree;
+	const { dx, dy } = direction;
+	let viewingDistance = 0;
+
+	// take your tree
+	// move the cursor in the given direction and test the next tree
+	function testNextTree(cursor) {
+		let { cx, cy } = cursor;
+		cx += dx;
+		cy += dy;
+
+		// has the cursor passed the edge of the forest?
+		if (
+			cy < 0 ||
+			cy === forest.length ||
+			cx < 0 ||
+			cx === forest[0].length
+		) {
+			return viewingDistance;
+		}
+
+		viewingDistance++;
+
+		// is the current tree the same or taller than the original tree?
+		if (forest[cy][cx] >= forest[ty][tx]) {
+			return viewingDistance;
+		}
+
+		// move the cursor to the next tree and test again
+		return testNextTree({ cx, cy });
+	}
+
+	return testNextTree({ cx: tx, cy: ty });
+}
+
+function getScenicScore(tree, forest) {
+	return (
+		getViewingDistanceInDirection(directions.up, tree, forest) *
+		getViewingDistanceInDirection(directions.down, tree, forest) *
+		getViewingDistanceInDirection(directions.left, tree, forest) *
+		getViewingDistanceInDirection(directions.right, tree, forest)
+	);
+}
+
+function getHighestScenicScoreInForest(forest) {
+	const scenicScores = [];
+	forEachTree(forest, (tree) => {
+		scenicScores.push(getScenicScore(tree, forest));
+	});
+
+	return Math.max(...scenicScores);
+}
+
 module.exports = {
 	countVisibleTreesInForest,
 	directions,
+	forEachTree,
 	getArrayOfForest,
+	getHighestScenicScoreInForest,
+	getScenicScore,
+	getViewingDistanceInDirection,
 	isVisibleInDirection
 };
--- day-8/test.js
+++ day-8/test.js
@@ -2,7 +2,11 @@ const { getData } = require("../util");
 const {
 	countVisibleTreesInForest,
 	directions,
+	forEachTree,
 	getArrayOfForest,
+	getScenicScore,
+	getHighestScenicScoreInForest,
+	getViewingDistanceInDirection,
 	isVisibleInDirection
 } = require("./go");
 let forest;
@@ -13,118 +17,376 @@ beforeAll(async () => {
 	);
 });
 
-describe("getArrayOfForest", () => {
-	test("returns 2-D array", () => {
-		const sampleForest = "123\n456";
-		const expectedArray = [
-			[1, 2, 3],
-			[4, 5, 6]
-		];
-		expect(getArrayOfForest(sampleForest)).toEqual(expectedArray);
-	});
-});
-
-describe("isVisibleInDirection()", () => {
-	describe("up", () => {
-		const testParams = [
-			{ tree: { tx: 1, ty: 1 }, result: true },
-			{ tree: { tx: 2, ty: 1 }, result: true },
-			{ tree: { tx: 3, ty: 4 }, result: true },
-			{ tree: { tx: 4, ty: 0 }, result: true },
-			{ tree: { tx: 0, ty: 1 }, result: false },
-			{ tree: { tx: 3, ty: 1 }, result: false },
-			{ tree: { tx: 3, ty: 1 }, result: false },
-			{ tree: { tx: 2, ty: 2 }, result: false }
-		];
-
-		testParams.forEach(({ tree, result }) => {
-			test("", () => {
-				expect(isVisibleInDirection(directions.up, tree, forest)).toBe(
-					result
-				);
-			});
+describe("utility functions", () => {
+	describe("getArrayOfForest", () => {
+		test("returns 2-D array", () => {
+			const sampleForest = "123\n456";
+			const expectedArray = [
+				[1, 2, 3],
+				[4, 5, 6]
+			];
+			expect(getArrayOfForest(sampleForest)).toEqual(expectedArray);
 		});
 	});
-	describe("down", () => {
-		const testParams = [
-			{ tree: { tx: 1, ty: 1 }, result: false },
-			{ tree: { tx: 4, ty: 0 }, result: false },
-			{ tree: { tx: 0, ty: 1 }, result: false },
-			{ tree: { tx: 0, ty: 2 }, result: true },
-			{ tree: { tx: 2, ty: 3 }, result: true },
-			{ tree: { tx: 3, ty: 4 }, result: true }
-		];
+	describe("forEachTree()", () => {
+		describe("iterates over every tree", () => {
+			const testParams = [
+				{
+					forest: [
+						[1, 1],
+						[1, 1]
+					],
+					treeCount: 4
+				},
+				{
+					forest: [
+						[1, 1, 1],
+						[1, 1, 1]
+					],
+					treeCount: 6
+				},
+				{
+					forest: [
+						[1, 1],
+						[1, 1],
+						[1, 1],
+						[1, 1]
+					],
+					treeCount: 8
+				},
+				{
+					forest: [
+						[1, 1, 1, 1],
+						[1, 1, 1, 1],
+						[1, 1, 1, 1]
+					],
+					treeCount: 12
+				}
+			];
 
-		testParams.forEach(({ tree, result }) => {
-			test("", () => {
-				expect(
-					isVisibleInDirection(directions.down, tree, forest)
-				).toBe(result);
-			});
-		});
-	});
-	describe("left", () => {
-		const testParams = [
-			{ tree: { tx: 1, ty: 0 }, result: false },
-			{ tree: { tx: 2, ty: 2 }, result: false },
-			{ tree: { tx: 4, ty: 4 }, result: false },
-			{ tree: { tx: 1, ty: 4 }, result: true },
-			{ tree: { tx: 2, ty: 3 }, result: true },
-			{ tree: { tx: 0, ty: 2 }, result: true }
-		];
-
-		testParams.forEach(({ tree, result }) => {
-			test("", () => {
-				expect(
-					isVisibleInDirection(directions.left, tree, forest)
-				).toBe(result);
-			});
-		});
-	});
-	describe("right", () => {
-		const testParams = [
-			{ tree: { tx: 1, ty: 0 }, result: false },
-			{ tree: { tx: 0, ty: 0 }, result: false },
-			{ tree: { tx: 0, ty: 4 }, result: false },
-			{ tree: { tx: 4, ty: 4 }, result: true },
-			{ tree: { tx: 1, ty: 2 }, result: true },
-			{ tree: { tx: 0, ty: 2 }, result: true }
-		];
-
-		testParams.forEach(({ tree, result }) => {
-			test("", () => {
-				expect(
-					isVisibleInDirection(directions.right, tree, forest)
-				).toBe(result);
+			testParams.forEach(({ forest, treeCount }) => {
+				test("", () => {
+					let count = 0;
+					forEachTree(forest, () => {
+						count++;
+					});
+					expect(count).toEqual(treeCount);
+				});
 			});
 		});
 	});
 });
 
-describe("countVisibleTreesInForest()", () => {
-	describe("edges are always visible", () => {
-		const testParams = [
-			{
-				forest: [
-					[1, 1],
-					[1, 1]
-				],
-				result: 4
-			}
-		];
+describe("problem 1", () => {
+	describe("isVisibleInDirection()", () => {
+		describe("up", () => {
+			const testParams = [
+				{ tree: { tx: 1, ty: 1 }, result: true },
+				{ tree: { tx: 2, ty: 1 }, result: true },
+				{ tree: { tx: 3, ty: 4 }, result: true },
+				{ tree: { tx: 4, ty: 0 }, result: true },
+				{ tree: { tx: 0, ty: 1 }, result: false },
+				{ tree: { tx: 3, ty: 1 }, result: false },
+				{ tree: { tx: 3, ty: 1 }, result: false },
+				{ tree: { tx: 2, ty: 2 }, result: false }
+			];
 
-		testParams.forEach(({ forest, result }) => {
-			test("", () => {
-				expect(countVisibleTreesInForest(forest)).toBe(result);
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						isVisibleInDirection(directions.up, tree, forest)
+					).toBe(result);
+				});
+			});
+		});
+		describe("down", () => {
+			const testParams = [
+				{ tree: { tx: 1, ty: 1 }, result: false },
+				{ tree: { tx: 4, ty: 0 }, result: false },
+				{ tree: { tx: 0, ty: 1 }, result: false },
+				{ tree: { tx: 0, ty: 2 }, result: true },
+				{ tree: { tx: 2, ty: 3 }, result: true },
+				{ tree: { tx: 3, ty: 4 }, result: true }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						isVisibleInDirection(directions.down, tree, forest)
+					).toBe(result);
+				});
+			});
+		});
+		describe("left", () => {
+			const testParams = [
+				{ tree: { tx: 1, ty: 0 }, result: false },
+				{ tree: { tx: 2, ty: 2 }, result: false },
+				{ tree: { tx: 4, ty: 4 }, result: false },
+				{ tree: { tx: 1, ty: 4 }, result: true },
+				{ tree: { tx: 2, ty: 3 }, result: true },
+				{ tree: { tx: 0, ty: 2 }, result: true }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						isVisibleInDirection(directions.left, tree, forest)
+					).toBe(result);
+				});
+			});
+		});
+		describe("right", () => {
+			const testParams = [
+				{ tree: { tx: 1, ty: 0 }, result: false },
+				{ tree: { tx: 0, ty: 0 }, result: false },
+				{ tree: { tx: 0, ty: 4 }, result: false },
+				{ tree: { tx: 4, ty: 4 }, result: true },
+				{ tree: { tx: 1, ty: 2 }, result: true },
+				{ tree: { tx: 0, ty: 2 }, result: true }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						isVisibleInDirection(directions.right, tree, forest)
+					).toBe(result);
+				});
 			});
 		});
 	});
+	describe("countVisibleTreesInForest()", () => {
+		describe("edges are always visible", () => {
+			const testParams = [
+				{
+					forest: [
+						[1, 1],
+						[1, 1]
+					],
+					result: 4
+				}
+			];
 
-	test("", () => {
-		expect(countVisibleTreesInForest(forest)).toBe(21);
+			testParams.forEach(({ forest, result }) => {
+				test("", () => {
+					expect(countVisibleTreesInForest(forest)).toBe(result);
+				});
+			});
+		});
+		test("", () => {
+			expect(countVisibleTreesInForest(forest)).toBe(21);
+		});
 	});
 });
 
-// get viewing distance of a given tree
+describe("problem 2", () => {
+	describe("getViewingDistanceInDirection()", () => {
+		describe("up", () => {
+			const testParams = [
+				{ tree: { tx: 0, ty: 0 }, result: 0 },
+				{ tree: { tx: 0, ty: 4 }, result: 1 },
+				{ tree: { tx: 4, ty: 0 }, result: 0 },
+				{ tree: { tx: 4, ty: 4 }, result: 1 },
+				{ tree: { tx: 2, ty: 1 }, result: 1 },
+				{ tree: { tx: 3, ty: 2 }, result: 2 }
+			];
 
-// get scenic score of a given tree
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						getViewingDistanceInDirection(
+							directions.up,
+							tree,
+							forest
+						)
+					).toBe(result);
+				});
+			});
+		});
+		describe("down", () => {
+			const testParams = [
+				{ tree: { tx: 0, ty: 0 }, result: 2 },
+				{ tree: { tx: 0, ty: 4 }, result: 0 },
+				{ tree: { tx: 4, ty: 0 }, result: 3 },
+				{ tree: { tx: 4, ty: 4 }, result: 0 },
+				{ tree: { tx: 2, ty: 1 }, result: 2 },
+				{ tree: { tx: 3, ty: 2 }, result: 1 }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						getViewingDistanceInDirection(
+							directions.down,
+							tree,
+							forest
+						)
+					).toBe(result);
+				});
+			});
+		});
+		describe("left", () => {
+			const testParams = [
+				{ tree: { tx: 0, ty: 0 }, result: 0 },
+				{ tree: { tx: 0, ty: 4 }, result: 0 },
+				{ tree: { tx: 4, ty: 0 }, result: 1 },
+				{ tree: { tx: 4, ty: 4 }, result: 1 },
+				{ tree: { tx: 2, ty: 1 }, result: 1 },
+				{ tree: { tx: 3, ty: 4 }, result: 3 }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						getViewingDistanceInDirection(
+							directions.left,
+							tree,
+							forest
+						)
+					).toBe(result);
+				});
+			});
+		});
+		describe("right", () => {
+			const testParams = [
+				{ tree: { tx: 0, ty: 0 }, result: 2 },
+				{ tree: { tx: 0, ty: 4 }, result: 1 },
+				{ tree: { tx: 4, ty: 0 }, result: 0 },
+				{ tree: { tx: 4, ty: 4 }, result: 0 },
+				{ tree: { tx: 2, ty: 1 }, result: 2 },
+				{ tree: { tx: 3, ty: 4 }, result: 1 }
+			];
+
+			testParams.forEach(({ tree, result }) => {
+				test("", () => {
+					expect(
+						getViewingDistanceInDirection(
+							directions.right,
+							tree,
+							forest
+						)
+					).toBe(result);
+				});
+			});
+		});
+	});
+	describe("getScenicScore()", () => {
+		const testParams = [
+			{ tree: { tx: 2, ty: 1 }, result: 4 },
+			{ tree: { tx: 2, ty: 3 }, result: 8 },
+			{ tree: { tx: 1, ty: 2 }, result: 6 },
+			{ tree: { tx: 0, ty: 0 }, result: 0 },
+			{ tree: { tx: 0, ty: 4 }, result: 0 },
+			{ tree: { tx: 4, ty: 0 }, result: 0 },
+			{ tree: { tx: 4, ty: 4 }, result: 0 }
+		];
+
+		testParams.forEach(({ tree, result }) => {
+			test("", () => {
+				expect(getScenicScore(tree, forest)).toBe(result);
+			});
+		});
+	});
+	describe("getHighestScenicScoreInForest()", () => {
+		test("test-data forest", () => {
+			expect(getHighestScenicScoreInForest(forest)).toBe(8);
+		});
+		test("production data forest", async () => {
+			const prodForest = await getData("./day-8/data.txt").then((data) =>
+				getArrayOfForest(data)
+			);
+			expect(getHighestScenicScoreInForest(prodForest)).toBe(504000);
+		});
+		test("large data forest", async () => {
+			const prodForest = await getData(
+				"./day-8/large-test-data.txt"
+			).then((data) => getArrayOfForest(data));
+			expect(getHighestScenicScoreInForest(prodForest)).toBe(2400);
+		});
+		describe("custom forests", () => {
+			const testParams = [
+				{
+					forest: [
+						[1, 2],
+						[3, 4]
+					],
+					result: 0
+				},
+				{
+					forest: [
+						[3, 0, 3, 7],
+						[2, 4, 5, 1],
+						[6, 5, 3, 3]
+					],
+					result: 2
+				},
+				{
+					forest: [
+						[1, 1, 1, 1],
+						[1, 4, 5, 1],
+						[1, 1, 1, 1],
+						[1, 1, 1, 1]
+					],
+					result: 4
+				},
+				{
+					forest: [
+						[1, 1, 1],
+						[1, 2, 1],
+						[1, 1, 1]
+					],
+					result: 1
+				},
+				{
+					forest: [
+						[1, 1, 1],
+						[1, 1, 1],
+						[1, 2, 1],
+						[1, 1, 1]
+					],
+					result: 2
+				},
+				{
+					forest: [
+						[1, 1, 1, 1],
+						[1, 1, 1, 1],
+						[1, 2, 1, 1],
+						[1, 1, 1, 1]
+					],
+					result: 4
+				},
+				{
+					forest: [
+						[1, 1, 1, 1],
+						[1, 1, 1, 1],
+						[1, 2, 1, 1],
+						[1, 1, 1, 1],
+						[1, 1, 1, 1]
+					],
+					result: 8
+				},
+				{
+					forest: [
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+						[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+					],
+					result: 625
+				}
+			];
+
+			testParams.forEach(({ forest, result }) => {
+				test("", () => {
+					expect(getHighestScenicScoreInForest(forest)).toBe(result);
+				});
+			});
+		});
+	});
+});
